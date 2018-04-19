60a233ab78ec7e9ab54aa644168c945012e34791
mnindrazaka <mnindrazaka@gmail.com>
2018-04-19 23:53:59 +0700

    make web api for jurusan

---
+++ app/Http/Controllers/Api/JurusanController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Jurusan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class JurusanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Jurusan::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nama' => 'required|string|min:5'
+        ]);
+
+        $data = Jurusan::create($request->all());
+        return response()->json($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = Jurusan::find($id);
+        return response()->json($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'nama' => 'required|string|min:5'
+        ]);
+
+        Jurusan::find($id)->update($request->all());
+        $data = Jurusan::find($id);
+        return response()->json($data);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Jurusan::destroy($id);
+        return response()->json([]);
+    }
+}
